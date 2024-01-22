edb54aef15241e9325545f0cf3ba24f87568ae8f
kyojin <kyojindev@gmail.com>
2024-01-22 11:12:29 -0400

    Fix logging

--- src/libs/common/log.cpp
+++ src/libs/common/log.cpp
@@ -19,33 +19,4 @@ void Log::init()
 #endif
 }
 
-void Log::log(Severity severity, std::string const &fmt...)
-{
-    std::time_t t = std::time(nullptr);
-
-    switch (severity)
-    {
-    case LOG_SEVERITY_TRACE:
-        fmt::print(fmt::fg(fmt::color::antique_white), "[{:%H:%M:%S}] TRACE: {}\n",
-                   fmt::localtime(t), fmt);
-        break;
-    case LOG_SEVERITY_INFO:
-        fmt::print(fmt::fg(fmt::color::white_smoke), "[{:%H:%M:%S}] INFO: {}\n", fmt::localtime(t),
-                   fmt);
-        break;
-    case LOG_SEVERITY_WARN:
-        fmt::print(fmt::fg(fmt::color::golden_rod), "[{:%H:%M:%S}] WARN: {}\n", fmt::localtime(t),
-                   fmt);
-        break;
-    case LOG_SEVERITY_ERROR:
-        fmt::print(fmt::fg(fmt::color::red), "[{:%H:%M:%S}] ERROR: {}\n", fmt::localtime(t), fmt);
-        break;
-    case LOG_SEVERITY_FATAL:
-        fmt::print(fmt::fg(fmt::color::red), "[{:%H:%M:%S}] FATAL: {}\n", fmt::localtime(t), fmt);
-        break;
-    default:
-        break;
-    }
-}
-
 } // namespace common
--- src/libs/common/log.hpp
+++ src/libs/common/log.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "fmt/chrono.h"
+#include "fmt/color.h"
+
 namespace common
 {
 
@@ -19,22 +22,56 @@ public:
 
     static void init();
 
-    static void log(Severity severity, std::string const &fmt...);
+    template <typename... Args>
+    static void log(Severity severity, std::string_view fmt, Args &&...args)
+    {
+        std::time_t t = std::time(nullptr);
+
+        auto msg = fmt::format(fmt, std::forward<Args>(args)...);
+
+        switch (severity)
+        {
+        case LOG_SEVERITY_TRACE:
+            fmt::print(fmt::fg(fmt::color::antique_white), "[{:%H:%M:%S}] TRACE: {}\n",
+                       fmt::localtime(t), msg);
+            break;
+        case LOG_SEVERITY_INFO:
+            fmt::print(fmt::fg(fmt::color::white_smoke), "[{:%H:%M:%S}] INFO: {}\n",
+                       fmt::localtime(t), msg);
+            break;
+        case LOG_SEVERITY_WARN:
+            fmt::print(fmt::fg(fmt::color::golden_rod), "[{:%H:%M:%S}] WARN: {}\n",
+                       fmt::localtime(t), msg);
+            break;
+        case LOG_SEVERITY_ERROR:
+            fmt::print(fmt::fg(fmt::color::crimson), "[{:%H:%M:%S}] ERROR: {}\n", fmt::localtime(t),
+                       msg);
+            break;
+        case LOG_SEVERITY_FATAL:
+            fmt::print(fmt::fg(fmt::color::crimson), "[{:%H:%M:%S}] FATAL: {}\n", fmt::localtime(t),
+                       msg);
+            break;
+        default:
+            break;
+        }
+    }
 
 private:
 };
 
 } // namespace common
 
-#define LOG_TRACE(...) ::common::Log::log(::common::Log::LOG_SEVERITY_TRACE, __VA_ARGS__)
-#define LOG_INFO(...) ::common::Log::log(::common::Log::LOG_SEVERITY_INFO, __VA_ARGS__)
-#define LOG_WARN(...) ::common::Log::log(::common::Log::LOG_SEVERITY_WARN, __VA_ARGS__)
-#define LOG_ERROR(...) ::common::Log::log(::common::Log::LOG_SEVERITY_ERROR, __VA_ARGS__)
-#define LOG_FATAL(...) ::common::Log::log(::common::Log::LOG_SEVERITY_FATAL, __VA_ARGS__)
+#define LOG_CALL(level, ...) ::common::Log::log(level, __VA_ARGS__)
+
+#define LOG_TRACE(...) LOG_CALL(::common::Log::LOG_SEVERITY_INFO, __VA_ARGS__)
+#define LOG_INFO(...) LOG_CALL(::common::Log::LOG_SEVERITY_INFO, __VA_ARGS__)
+#define LOG_WARN(...) LOG_CALL(::common::Log::LOG_SEVERITY_WARN, __VA_ARGS__)
+#define LOG_ERROR(...) LOG_CALL(::common::Log::LOG_SEVERITY_ERROR, __VA_ARGS__)
+#define LOG_FATAL(...) LOG_CALL(::common::Log::LOG_SEVERITY_FATAL, __VA_ARGS__)
 
 #define LOG_ASSERT(condition, ...)                                                                 \
     if (!(condition))                                                                              \
     {                                                                                              \
-        ::common::Log::log(::common::Log::LOG_SEVERITY_FATAL, __VA_ARGS__);                        \
+        LOG_CALL(::common::Log::LOG_SEVERITY_FATAL, __VA_ARGS__);                                  \
         throw std::runtime_error("Assertion failed");                                              \
     }
